ee036a16428decc18909ab5ac1a80f785000ae44
Emiliano <emilianoloredo@gmail.com>
2026-09-10 16:01:58 -0300

    Agregar credenciales Firebase

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_zit-XAgpuxbZqXKwE6Ed9U4N_U3RklY",
+  authDomain: "casa-de-comidas-app.firebaseapp.com",
+  projectId: "casa-de-comidas-app",
+  storageBucket: "casa-de-comidas-app.firebasestorage.app",
+  messagingSenderId: "282307754682",
+  appId: "1:282307754682:web:e0a5c689b0c8782f15a083"
+};
+
+// Inicializar Firebase (lo usaremos después)
+// firebase.initializeApp(firebaseConfig);
